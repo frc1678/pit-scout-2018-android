93962a85d44e2ccc55ab9cdf873229967af51628
JudeManansala <jademon2002@yahoo.com>
2018-04-04 19:21:13 -0700

    Notes save and send to firebase

--- app/src/main/java/com/jadem/androidpitscout/ViewTeam.java
+++ app/src/main/java/com/jadem/androidpitscout/ViewTeam.java
@@ -1,21 +1,26 @@
 package com.jadem.androidpitscout;
 
 
+import android.app.SearchManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -61,6 +66,23 @@ public class ViewTeam extends AppCompatActivity {
 
         SEALsNotesEditText.setFocusable(true);
 
+        dataBaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                if(dataSnapshot.getValue() != null) {
+                    SEALsNotesEditText.setText(dataSnapshot.getValue().toString());
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e("Error", "trialEventListener Cancelled");
+                Toast connectionErrorToast = Toast.makeText(getApplicationContext(), "Connection Error", Toast.LENGTH_SHORT);
+                connectionErrorToast.setGravity(Gravity.CENTER, 0, 0);
+                connectionErrorToast.show();
+            }
+        });
     }
 
     public void sendNotes(View view) {
